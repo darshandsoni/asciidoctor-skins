491afd47db104a68b7334c4f2d3467322b97b7e3
dohliam <dohliam@gmail.com>
2016-11-28 06:29:41 -0800

    add tags for all showcase themes

--- screenshots/objects.js
+++ screenshots/objects.js
@@ -1,136 +1,136 @@
 var inputstream = [{
   "name": "default",
   "image": "png/default.png",
-  "tags": "",
+  "tags": "light,sidebar",
   "description": "The default asciidoctor stylesheet"
 }, {
   "name": "boot-cerulean",
   "image": "png/boot-cerulean.png",
-  "tags": "blue, fullwidth, navbar, rounded",
+  "tags": "blue,fullwidth,navbar,rounded",
   "description": "Based on Bootswatch Cerulean: 'A calm blue sky'"
 }, {
   "name": "boot-cosmo",
   "image": "png/boot-cosmo.png",
-  "tags": "blue, fullwidth, navbar",
+  "tags": "blue,fullwidth,navbar",
   "description": "Based on Bootswatch Cosmo: 'An ode to Metro'"
 }, {
   "name": "boot-cyborg",
   "image": "png/boot-cyborg.png",
-  "tags": "dark, fullwidth, navbar",
+  "tags": "dark,fullwidth,navbar",
   "description": "Based on Bootswatch Cyborg: 'Jet black and electric blue'"
 }, {
   "name": "boot-darkly",
   "image": "png/boot-darkly.png",
-  "tags": "dark, fullwidth, navbar",
+  "tags": "dark,fullwidth,navbar",
   "description": "Based on Bootswatch Darkly: 'Flatly in night mode'"
 }, {
   "name": "boot-flatly",
   "image": "png/boot-flatly.png",
-  "tags": "light, fullwidth, navbar",
+  "tags": "light,fullwidth,navbar",
   "description": "Based on Bootswatch Flatly: 'Flat and modern'"
 }, {
   "name": "boot-journal",
   "image": "png/boot-journal.png",
-  "tags": "light, fullwidth, navbar, newspaper",
+  "tags": "light,fullwidth,navbar,newspaper",
   "description": "Based on Bootswatch Journal: 'Crisp like a new sheet of paper'"
 }, {
   "name": "boot-lumen",
   "image": "png/boot-lumen.png",
-  "tags": "light, fullwidth, navbar",
+  "tags": "light,fullwidth,navbar",
   "description": "Based on Bootswatch Lumen: 'Light and shadow'"
 }, {
   "name": "boot-paper",
   "image": "png/boot-paper.png",
-  "tags": "light, fullwidth, navbar",
+  "tags": "light,fullwidth,navbar",
   "description": "Based on Bootswatch Paper: 'Material is the metaphor'"
 }, {
   "name": "boot-readable",
   "image": "png/boot-readable.png",
-  "tags": "light, fullwidth, navbar, blue",
+  "tags": "light,fullwidth,navbar,blue",
   "description": "Based on Bootswatch Readable: 'Optimized for legibility'"
 }, {
   "name": "boot-sandstone",
   "image": "png/boot-sandstone.png",
-  "tags": "light, fullwidth, navbar, rounded",
+  "tags": "light,fullwidth,navbar,rounded",
   "description": "Based on Bootswatch Sandstone: 'A touch of warmth'"
 }, {
   "name": "boot-slate",
   "image": "png/boot-slate.png",
-  "tags": "dark, fullwidth, navbar, grey",
+  "tags": "dark,fullwidth,navbar,grey",
   "description": "Based on Bootswatch Slate: 'Shades of gunmetal gray'"
 }, {
   "name": "boot-spacelab",
   "image": "png/boot-spacelab.png",
-  "tags": "light, fullwidth, navbar",
+  "tags": "light,fullwidth,navbar",
   "description": "Based on Bootswatch Spacelab: 'Silvery and sleek'"
 }, {
   "name": "boot-superhero",
   "image": "png/boot-superhero.png",
-  "tags": "dark, fullwidth, navbar, blue",
+  "tags": "dark,fullwidth,navbar,blue",
   "description": "Based on Bootswatch Superhero: 'The brave and the blue'"
 }, {
   "name": "boot-yeti",
   "image": "png/boot-yeti.png",
-  "tags": "light, fullwidth, navbar",
+  "tags": "light,fullwidth,navbar",
   "description": "Based on Bootswatch Yeti: 'A friendly foundation'"
 }, {
   "name": "clean",
   "image": "png/clean.png",
-  "tags": "",
+  "tags": "light,sidebar",
   "description": "A clean, sans serif adaptation of the default"
 }, {
   "name": "dark",
   "image": "png/dark.png",
-  "tags": "",
+  "tags": "dark,sidebar",
   "description": "A dark theme"
 }, {
   "name": "fedora",
   "image": "png/fedora.png",
-  "tags": "",
+  "tags": "light,sidebar,foss,grey",
   "description": "A theme based on Fedora Linux"
 }, {
   "name": "gazette",
   "image": "png/gazette.png",
-  "tags": "",
+  "tags": "light,sidebar,newspaper",
   "description": "A theme with a newspaper feel"
 }, {
   "name": "italian-pop",
   "image": "png/italian-pop.png",
-  "tags": "",
-  "description": ""
+  "tags": "light,sidebar,grey,red",
+  "description": "A stylish, effervescent theme"
 }, {
   "name": "material",
   "image": "png/material.png",
-  "tags": "",
+  "tags": "light,sidebar,flat,green",
   "description": "Based on Google's Material style guidelines"
 }, {
   "name": "medium",
   "image": "png/medium.png",
-  "tags": "",
+  "tags": "light,nomenu,website",
   "description": "Make your documentation look like a website"
 }, {
   "name": "notebook",
   "image": "png/notebook.png",
-  "tags": "",
+  "tags": "light,sidebar,paper,lines,handwriting,grey",
   "description": "A skeuomorphic notepaper theme"
 }, {
   "name": "plain",
   "image": "png/plain.png",
-  "tags": "",
+  "tags": "light,sidebar,grey",
   "description": "An elegant plain theme"
 }, {
   "name": "template",
   "image": "png/template.png",
-  "tags": "",
+  "tags": "test",
   "description": "An example theme highlighting all available elements for styling"
 }, {
   "name": "tufte",
   "image": "png/tufte.png",
-  "tags": "",
+  "tags": "light,nomenu",
   "description": "A theme based on Edward Tufte's work"
 }, {
   "name": "ubuntu",
   "image": "png/ubuntu.png",
-  "tags": "",
+  "tags": "light,sidebar,foss,orange,purple",
   "description": "A theme based on Ubuntu Linux"
 }];
