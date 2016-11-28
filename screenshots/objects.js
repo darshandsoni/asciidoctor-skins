bf879f83eb1222aaee4203aef6539dfee1ea1595
dohliam <dohliam@gmail.com>
2016-11-28 06:16:08 -0800

    add screenshots for new themes

--- screenshots/objects.js
+++ screenshots/objects.js
@@ -4,10 +4,75 @@ var inputstream = [{
   "tags": "",
   "description": "The default asciidoctor stylesheet"
 }, {
+  "name": "boot-cerulean",
+  "image": "png/boot-cerulean.png",
+  "tags": "blue, fullwidth, navbar, rounded",
+  "description": "Based on Bootswatch Cerulean: 'A calm blue sky'"
+}, {
   "name": "boot-cosmo",
   "image": "png/boot-cosmo.png",
-  "tags": "bootswatch, blue, fullwidth, navbar",
+  "tags": "blue, fullwidth, navbar",
   "description": "Based on Bootswatch Cosmo: 'An ode to Metro'"
+}, {
+  "name": "boot-cyborg",
+  "image": "png/boot-cyborg.png",
+  "tags": "dark, fullwidth, navbar",
+  "description": "Based on Bootswatch Cyborg: 'Jet black and electric blue'"
+}, {
+  "name": "boot-darkly",
+  "image": "png/boot-darkly.png",
+  "tags": "dark, fullwidth, navbar",
+  "description": "Based on Bootswatch Darkly: 'Flatly in night mode'"
+}, {
+  "name": "boot-flatly",
+  "image": "png/boot-flatly.png",
+  "tags": "light, fullwidth, navbar",
+  "description": "Based on Bootswatch Flatly: 'Flat and modern'"
+}, {
+  "name": "boot-journal",
+  "image": "png/boot-journal.png",
+  "tags": "light, fullwidth, navbar, newspaper",
+  "description": "Based on Bootswatch Journal: 'Crisp like a new sheet of paper'"
+}, {
+  "name": "boot-lumen",
+  "image": "png/boot-lumen.png",
+  "tags": "light, fullwidth, navbar",
+  "description": "Based on Bootswatch Lumen: 'Light and shadow'"
+}, {
+  "name": "boot-paper",
+  "image": "png/boot-paper.png",
+  "tags": "light, fullwidth, navbar",
+  "description": "Based on Bootswatch Paper: 'Material is the metaphor'"
+}, {
+  "name": "boot-readable",
+  "image": "png/boot-readable.png",
+  "tags": "light, fullwidth, navbar, blue",
+  "description": "Based on Bootswatch Readable: 'Optimized for legibility'"
+}, {
+  "name": "boot-sandstone",
+  "image": "png/boot-sandstone.png",
+  "tags": "light, fullwidth, navbar, rounded",
+  "description": "Based on Bootswatch Sandstone: 'A touch of warmth'"
+}, {
+  "name": "boot-slate",
+  "image": "png/boot-slate.png",
+  "tags": "dark, fullwidth, navbar, grey",
+  "description": "Based on Bootswatch Slate: 'Shades of gunmetal gray'"
+}, {
+  "name": "boot-spacelab",
+  "image": "png/boot-spacelab.png",
+  "tags": "light, fullwidth, navbar",
+  "description": "Based on Bootswatch Spacelab: 'Silvery and sleek'"
+}, {
+  "name": "boot-superhero",
+  "image": "png/boot-superhero.png",
+  "tags": "dark, fullwidth, navbar, blue",
+  "description": "Based on Bootswatch Superhero: 'The brave and the blue'"
+}, {
+  "name": "boot-yeti",
+  "image": "png/boot-yeti.png",
+  "tags": "light, fullwidth, navbar",
+  "description": "Based on Bootswatch Yeti: 'A friendly foundation'"
 }, {
   "name": "clean",
   "image": "png/clean.png",
