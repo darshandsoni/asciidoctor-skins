9907e107fada25801b1aeacbe2813aa908f9d831
dohliam <dohliam@gmail.com>
2019-11-11 23:44:46 -0800

    refactor switcher.js to acommodate pages with multiple stylesheets

--- switcher.js
+++ switcher.js
@@ -1,9 +1,12 @@
-var skins = ["asciidoctor", "boot-cerulean", "boot-cosmo", "boot-cyborg", "boot-darkly", "boot-flatly", "boot-journal", "boot-lumen", "boot-paper", "boot-readable", "boot-slate", "boot-spacelab", "boot-superhero", "boot-yeti", "clean", "dark", "fedora", "gazette", "italian-pop", "material-amber", "material-blue", "material-brown", "material-green", "material-grey", "material-orange", "material-pink", "material-purple", "material-red", "material-teal", "medium", "monospace", "notebook", "plain", "template", "tufte", "ubuntu"]
+var skins = ["asciidoctor", "clean", "dark", "fedora", "gazette", "italian-pop", "medium", "monospace", "notebook", "plain", "tufte", "ubuntu", "material-amber", "material-blue", "material-brown", "material-green", "material-grey", "material-orange", "material-pink", "material-purple", "material-red", "material-teal", "boot-cerulean", "boot-cosmo", "boot-cyborg", "boot-darkly", "boot-flatly", "boot-journal", "boot-lumen", "boot-paper", "boot-readable", "boot-slate", "boot-spacelab", "boot-superhero", "boot-yeti", "template"]
 
-add_switcher();
+purgeCss();
+addSwitcher();
 
-function switch_css(css) {
-  document.getElementsByTagName("link")[0].href = "https://darshandsoni.com/asciidoctor-skins/css/" + css + ".css";
+function switchCss(css) {
+  link = document.getElementsByTagName("link");
+  last = link[link.length - 1];
+  last.href = "https://darshandsoni.com/asciidoctor-skins/css/" + css + ".css";
 }
 
 function capitalize(s) {
@@ -13,9 +16,9 @@ function capitalize(s) {
   return u;
 }
 
-function inline_switcher() {
+function inlineSwitcher() {
   switcher = document.getElementById("switcher");
-  select_open = '\n        <select name="switcher_dropdown" id="switcher_dropdown" accesskey="s" onchange="switch_css(this.value)">\n';
+  select_open = '\n        <select name="switcher_dropdown" id="switcher_dropdown" accesskey="s" onchange="switchCss(this.value)">\n';
   dropdown = select_open;
   for (i = 0; i < skins.length; i++) {
     f = skins[i];
@@ -28,21 +31,54 @@ function inline_switcher() {
   switcher.innerHTML = dropdown;
 }
 
-function add_switcher() {
-  stylesheet = document.getElementsByTagName("link")[0];
-  if (stylesheet == undefined) {
-    head = document.getElementsByTagName('head')[0];
-    link = document.createElement('link');
-    link.rel="stylesheet";
-    link.type="text/css";
-    link.href="https://darshandsoni.com/asciidoctor-skins/css/" + skins[0] + ".css";
-    head.appendChild(link);
-  }
+function addSwitcher() {
+  head = document.getElementsByTagName('head')[0];
+  link = document.createElement('link');
+  link.rel="stylesheet";
+  link.type="text/css";
+  link.href="https://darshandsoni.com/asciidoctor-skins/css/" + skins[0] + ".css";
+  head.appendChild(link);
 
   var header = document.getElementById("header");
   var new_div = document.createElement("div");
-  new_div.innerHTML = '<div id="switcher">&nbsp;</div>\n      <script type="text/javascript">inline_switcher();</script>';
+  new_div.innerHTML = '<div id="switcher">&nbsp;</div>\n      <script type="text/javascript">inlineSwitcher();</script>';
   window.header.insertBefore(new_div, window.header.firstChild);
 
-  inline_switcher();
+  inlineSwitcher();
+}
+
+function removeDefault() {
+  var styles = document.getElementsByTagName("style");
+  for (var i = 0; i < styles.length; i++) {
+    if (/\* Asciidoctor default stylesheet/.test(styles[i].textContent)) {
+      removeNode(styles[i]);
+    }
+  }
+}
+
+function removeClean() {
+  var links = document.getElementsByTagName("link");
+  for (var i = 0; i < links.length; i++) {
+    if (/clean\.css/.test(links[i].href)) {
+      removeNode(links[i]);
+    }
+  }
+}
+
+function isEmpty(obj) {
+  for(var prop in obj) {
+    if(obj.hasOwnProperty(prop)) {
+      return false;
+    }
+  }
+  return true;
+}
+
+function removeNode(n) {
+  n.parentNode.removeChild(n);
+}
+
+function purgeCss() {
+  removeDefault();
+  removeClean();
 }
